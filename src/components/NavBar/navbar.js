78b6d4f2c4b696b4307575e912f907f86cea38e6
Prabigya Thapa <thapaprabigya@gmail.com>
2023-11-15 17:38:45 -0700

    More updates on nav bar

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import './navbar.css'
 import logo from '../../resources/logo.png'
+import contact from '../../resources/contact.png'
+
 import { Link } from 'react-scroll'
 
 export const Navbar = () => {
   return (
     <nav className = "navbar">
-        <img src={logo} alt="logo"/>
+        <img src={logo} alt="logo" className='logo'/>
         <div className='desktopMenu'>
+            <Link className='desktopMenuListItem'>Home</Link>
+            <Link className='desktopMenuListItem'>About</Link>
+            <Link className='desktopMenuListItem'>Portfolio</Link>
+            <Link className='desktopMenuListItem'>Personal Projects</Link>
 
         </div>
         <button className='desktopMenuBtn'>
-        <img src="" alt="" className='desltopMenuImg'/>Contact Me
+        <img src={contact} alt="contactLogo" className='desktopMenuImg'/>Contact Me
         </button>
     </nav>
   )
